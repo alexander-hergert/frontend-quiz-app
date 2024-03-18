5070dff3714575e2cfd1b9fa60cb3a4260e82875
Alexander Hergert <alexander.hergert1989@gmail.com>
2024-03-18 16:11:04 +0100

    Render single questions and add buttons.

--- src/components/HtmlQuiz.jsx
+++ src/components/HtmlQuiz.jsx
@@ -1,32 +1,59 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const HtmlQuiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(1);
   return (
     <>
-      {questions.map((question, index) => (
-        <div key={index} className="flex gap-4 justify-between border">
-          <div>
-            <div>
-              Question {index + 1 + " / "} {questions.length}
+      {questions.map(
+        (question, index) =>
+          currentQuestion === index + 1 && (
+            <div key={index} className="flex gap-4 justify-between border">
+              <div>
+                <div>
+                  Question {currentQuestion} / {questions.length}
+                </div>
+                <div>{question?.question}</div>
+                <progress
+                  className="progress progress-primary w-56"
+                  value={currentQuestion}
+                  max="10"
+                ></progress>
+              </div>
+              <form className="flex flex-col">
+                <ul>
+                  {question?.options.map((option, optionIndex) => (
+                    <li key={optionIndex}>
+                      <input
+                        id={`question-${index}-${optionIndex}`}
+                        type="radio"
+                        name={`question-${index}`}
+                      />
+                      <label htmlFor={`question-${index}-${optionIndex}`}>
+                        {option}
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+                <button>Submit</button>
+              </form>
             </div>
-            <div>{question?.question}</div>
-            <progress
-              className="progress progress-primary w-56"
-              value={index + 1}
-              max="10"
-            ></progress>
-          </div>
-          <form className="flex flex-col">
-            <ul>
-              {question?.options.map((option, index) => (
-                <li key={index}>{option}</li>
-              ))}
-            </ul>
-            <button>Submit</button>
-          </form>
-        </div>
-      ))}
+          )
+      )}
+      <div>
+        <button
+          className="btn"
+          onClick={() => setCurrentQuestion(currentQuestion - 1)}
+        >
+          BACK
+        </button>
+        <button
+          className="btn"
+          onClick={() => setCurrentQuestion(currentQuestion + 1)}
+        >
+          FORWARD
+        </button>
+      </div>
     </>
   );
 };
